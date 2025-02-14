dc14c95e58e1fe1075a10a87fe72e2af3af6d577
onionbulbperception <unionperception@protonmail.com>
2025-02-14 09:06:45 +0200

    finished tutorial's part 13

--- scripts/room_manager.gd
+++ scripts/room_manager.gd
@@ -4,9 +4,22 @@ extends Node
 func _ready() -> void:
 	var key = Item.new()
 	key.initialize("a key", Types.ItemTypes.KEY)
-	key.use_value = $ShedRoom
 	
-	$HouseRoom.connect_exit_unlocked("east", $OutsideRoom)
+	$ShedRoom.connect_exit_unlocked("west", $BackOfInnRoom)
 	
-	$OutsideRoom.add_item(key)
-	$OutsideRoom.connect_exit_locked("north", $ShedRoom)
+	$BackOfInnRoom.connect_exit_unlocked("path", $VillageSquareRoom)
+	
+	$VillageSquareRoom.connect_exit_unlocked("east", $InnRoom)
+	$VillageSquareRoom.connect_exit_unlocked("north", $TownGateRoom)
+	$VillageSquareRoom.connect_exit_unlocked("west", $FieldRoom)
+	
+	$InnDoorRoom.connect_exit_unlocked("inside", $InnRoom)
+	
+	$InnRoom.connect_exit_unlocked("south", $"InnsKitchenRoom")
+	
+	var exit = $InnsKitchenRoom.connect_exit_locked("south", $BackOfInnRoom)
+	key.use_value = exit
+	$InnsKitchenRoom.add_item(key)
+	
+	$TownGateRoom.connect_exit_unlocked("forest", $ForestRoom, "gate")
+	
