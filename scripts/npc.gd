dc7205fbd05435e7fe8919f3435b09d57b57f016
onionbulbperception <unionperception@protonmail.com>
2025-02-15 15:04:45 +0200

    finished tutorial's part 15

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -3,5 +3,10 @@ class_name NPC
 
 
 @export var npc_name : String = "NPC Name"
+@export var quest_item : Resource
 @export_multiline var initial_dialog: String
 @export_multiline var post_quest_dialog: String
+
+
+var has_received_quest_item := false
+var quest_reward = null
--- scripts/room_manager.gd
+++ scripts/room_manager.gd
@@ -10,6 +10,10 @@ func _ready() -> void:
 	$VillageSquareRoom.connect_exit_unlocked("north", $TownGateRoom)
 	$VillageSquareRoom.connect_exit_unlocked("west", $FieldRoom)
 	
+	var sword = load_item("guard_sword")
+	#sword.use_value = exit
+	$FieldRoom.add_item(sword)
+	
 	$InnDoorRoom.connect_exit_unlocked("inside", $InnRoom)
 	
 	var innkeeper = load_npc("innkeeper")
@@ -18,13 +22,14 @@ func _ready() -> void:
 
 	
 	var exit = $InnsKitchenRoom.connect_exit_locked("south", $BackOfInnRoom)
-	var key = load_item("key")
+	var key = load_item("inn_kitchen_key")
 	key.use_value = exit
 	$InnsKitchenRoom.add_item(key)
 	
+	exit = $TownGateRoom.connect_exit_locked("forest", $ForestRoom, "gate")
 	var guard = load_npc("guard")
 	$TownGateRoom.add_npc(guard)
-	$TownGateRoom.connect_exit_unlocked("forest", $ForestRoom, "gate")
+	guard.quest_reward = exit
 
 
 func load_item(item_name: String):
